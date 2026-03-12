b6d0b5edc32b3b9840cba360b52b6a12f9ec34ba
ThiagoCosta <thiagoacnascimento@gmail.com>
2026-03-12 11:24:28 -0400

    refactor: remove basepath e recoloca assetprefix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/semec/form-itbi-imunidade",
+  assetPrefix: "/semec/form-itbi-imunidade",
   images: {
     unoptimized: true,
   },
