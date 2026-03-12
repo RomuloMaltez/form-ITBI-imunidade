73c8c4f7a6a7781ae67a830341463654f98c51c4
ThiagoCosta <thiagoacnascimento@gmail.com>
2026-03-12 10:57:34 -0400

    refactor: muda url

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/_next/:path*",
-        destination: "https://form-itbi-imunidade.vercel.app/_next/:path*",
-      },
-      {
-        source: "/:path*",
-        destination: "https://form-itbi-imunidade.vercel.app/:path*",
-      },
-    ];
-  },
+  assetPrefix: "/semec/form-itbi-imunidade",
 };
 
 export default nextConfig;
