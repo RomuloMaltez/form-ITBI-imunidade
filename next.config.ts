68ae26e74040bb03d34d544a63a76759614dbe14
ThiagoCosta <thiagoacnascimento@gmail.com>
2026-03-12 11:38:14 -0400

    refactor: change the url of each header img

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/semec/form-itbi-imunidade",
   assetPrefix: "/semec/form-itbi-imunidade",
   images: {
     unoptimized: true,
