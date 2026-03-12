370392c9d0c8f9cf0af1b76f70b5aa3c6b226ab9
ThiagoCosta <thiagoacnascimento@gmail.com>
2026-03-12 11:10:36 -0400

    refactor: muda url

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   assetPrefix: "/semec/form-itbi-imunidade",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
